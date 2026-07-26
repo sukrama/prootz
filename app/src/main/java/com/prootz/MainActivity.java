f485dd21f72e027c8619da10fb573df5738ea11c
sukrama <sukramasza@gmail.com>
2026-07-26 18:23:20 +0700

    Update MainActivity.java

--- app/src/main/java/com/prootz/MainActivity.java
+++ app/src/main/java/com/prootz/MainActivity.java
@@ -233,6 +233,7 @@ public class MainActivity extends Activity {
         java.util.ArrayList<String> args = new java.util.ArrayList<>();
         args.add(prootExec.getAbsolutePath());
         args.add("--root-id");
+        args.add("--link2symlink");
         args.add("--kill-on-exit");
         args.add("--sysvipc");
         args.add("-L");
